ef033c8c39f49175cd18e21b21393db96c4e58b7
murat <murat@murats-MacBook-Pro.local>
2020-08-08 15:52:37 +0300

    updating build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 fn main(){
     println!(r"cargo:rustc-link-search=/usr/local/Cellar/gcc/10.2.0/lib/gcc/10");
     println!(r"cargo:rustc-link-search=/usr/local/opt/openblas/lib");
-
-    // /usr/local/opt/lapack/lib
 }
